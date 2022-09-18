f933595d40ca33120cf9d48a2c7be3392d59ed4a
johnmanjiro13 <28798279+johnmanjiro13@users.noreply.github.com>
2022-09-18 09:42:07 +0900

    refactor: Separate utils to print and repository

--- repository.go
+++ repository.go
@@ -1,11 +1,8 @@
 package cmcm
 
 import (
-	"bufio"
-	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
 	"strings"
 
 	"github.com/cli/go-gh"
@@ -42,42 +39,4 @@ func resolveRepository() (owner, repo string, err error) {
 	owner = ownerRepo[0]
 	repo = ownerRepo[1]
 	return owner, repo, nil
-}
-
-func printPlain(w io.Writer, cmt ...*Comment) error {
-	bw := bufio.NewWriter(w)
-	for i, c := range cmt {
-		var s string
-		if i > 0 {
-			s = "----------------------------\n"
-		}
-		s = s + fmt.Sprintln("ID:\t", c.ID)
-		s = s + fmt.Sprintln("Author:\t", c.Author)
-		s = s + fmt.Sprintln("URL:\t", c.HTMLURL)
-		s = s + fmt.Sprintln("")
-		s = s + fmt.Sprintln(c.Body)
-		if _, err := bw.Write([]byte(s)); err != nil {
-			return err
-		}
-	}
-
-	return bw.Flush()
-}
-
-func printJSON(w io.Writer, cmt ...*Comment) error {
-	var s []byte
-	var err error
-	if len(cmt) == 1 {
-		s, err = json.Marshal(cmt[0])
-	} else {
-		s, err = json.Marshal(cmt)
-	}
-	if err != nil {
-		return fmt.Errorf("failed to marshal: %w", err)
-	}
-	bw := bufio.NewWriter(w)
-	if _, err := bw.Write(s); err != nil {
-		return err
-	}
-	return bw.Flush()
 }
